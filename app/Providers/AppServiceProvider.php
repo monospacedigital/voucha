9c4fed6b5b68350c8095c3a3cd6983e9f5f781ca
monospacedigital <66475547+monospacedigital@users.noreply.github.com>
2025-02-24 13:14:41 +0100

    edited database.php, queue.php, and AppServiceProvider.php to mysql for deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+
+        if (config('database.default') === 'mysql') {
+            DB::statement('SET SESSION sql_require_primary_key=0');
+        }
     }
 }
